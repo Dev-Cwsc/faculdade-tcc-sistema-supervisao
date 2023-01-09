439b34eaf1336e52c07308e161f60f6e81699651
Carlos Camargos <cwscamargos@gmail.com>
2023-01-08 21:08:27 -0300

    First monitor interface finished
    Finished the first version of monitor interface.

--- src/views/pages/Monitor.jsx
+++ src/views/pages/Monitor.jsx
@@ -1,5 +1,5 @@
 import "../styles/pages.css";
-import { useState } from "react";
+//import { useState } from "react";
 import Navbar from "../components/Navbar";
 
 function Monitor() {
@@ -9,7 +9,29 @@ function Monitor() {
     <div className="container">
       <Navbar />
       <div className="container-monitor">
-        <div className="container-elements"></div>
+        <div className="container-elements">
+          <div className="wrapper-elements">
+            <div className="monitor-element">
+              <label className="txt-element"> Biblioteca (700W) </label>
+            </div>
+            <div className="monitor-element-spacing"></div>
+            <div className="monitor-element">
+              <label className="txt-element"> Refeitório (Offline) </label>
+            </div>
+            <div className="monitor-element-spacing"></div>
+            <div className="monitor-element">
+              <label className="txt-element"> Alojamento (Offline) </label>
+            </div>
+            <div className="monitor-element-spacing"></div>
+            <div className="monitor-element">
+              <label className="txt-element"> Prédio Pedagógico (Offline) </label>
+            </div>
+            <div className="monitor-element-spacing"></div>
+            <div className="monitor-element">
+              <label className="txt-element"> Centro de Convivência (Offline) </label>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
